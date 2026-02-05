a8b015759b888039df660270cf0afbafa9ba9c93
EC2 Default User <ec2-user@ip-172-31-22-106.ec2.internal>
2026-02-05 15:52:37 +0000

    Fix CodeDeploy failure by cleaning old WAR before deploy

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "Stopping services..."
+echo "Stopping services and cleaning old deployment"
+
+if systemctl is-active --quiet tomcat; then
+  sudo systemctl stop tomcat
+fi
 
 if systemctl is-active --quiet httpd; then
   sudo systemctl stop httpd
 fi
 
-if systemctl is-active --quiet tomcat; then
-  sudo systemctl stop tomcat
-fi
+sudo rm -f /usr/share/tomcat/webapps/nextwork-web-project.war
+sudo rm -rf /usr/share/tomcat/webapps/nextwork-web-project
